c607ea971695d56766779a8c3b140ca9b19e9f31
unknown <primes461@gmail.com>
2025-07-19 18:18:49 +0530

    code

---
+++ testvm2/main.tf
@@ -0,0 +1,96 @@
+
+# 1. Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-todo"
+  location = "Central India"
+}
+
+# 2. Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-linux"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# 3. Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "fronend-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# 4. Public IP
+resource "azurerm_public_ip" "public_ip" {
+  name                = "linuxvm-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# 5. Network Interface
+resource "azurerm_network_interface" "nic" {
+  name                = "linuxvm-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+# 6. Linux Virtual Machine
+resource "azurerm_linux_virtual_machine" "linux_vm" {
+  name                            = "linuxvm"
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  size                            = "Standard_B1s"
+  admin_username                  = "adminuser"
+  admin_password                  = "admin@123456"
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    name                 = "linuxvm-osdisk"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  tags = {
+    environment = "Test"
+  }
+}
+
+# NSG rule port TCP:- 22 allowd
+resource "azurerm_network_security_group" "nsg" {
+  name                = "linuxvm-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
